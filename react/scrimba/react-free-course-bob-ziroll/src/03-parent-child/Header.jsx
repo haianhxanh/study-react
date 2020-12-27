2a9f6261db36c9d107792246cba219b688e91367
haianhxanh <hanka7n@gmail.com>
2020-12-27 16:22:13 +0100

    remove from master

--- react/scrimba/react-free-course-bob-ziroll/src/03-parent-child/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Header() {
-  return (
-  <h1>03. Parent and Child in React</h1>
-  )
-}
-
-export default Header;
--- react/scrimba/react-free-course-bob-ziroll/src/03-parent-child/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Navbar() {
-  return (
-  <p>This is a Navigation bar</p>
-  )
-}
-
-export default Navbar;
