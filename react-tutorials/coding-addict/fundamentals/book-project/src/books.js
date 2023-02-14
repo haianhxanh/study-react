9eee100f45fddeb0e53bdf740edfbe2372cb3332
Hai Anh <67957887+haianhxanh@users.noreply.github.com>
2023-02-14 22:15:04 +0100

    Local images

--- react-tutorials/coding-addict/fundamentals/book-project/src/books.js
+++ react-tutorials/coding-addict/fundamentals/book-project/src/books.js
@@ -1,16 +1,25 @@
+import img1 from "./images/book1.jpg";
+import img2 from "./images/book2.jpg";
+import img3 from "./images/book3.jpg";
+
 export const books = [
   {
     img: "https://images-na.ssl-images-amazon.com/images/I/71hTq4t1PEL._AC_UL200_SR200,200_.jpg",
     title: "Let That Sh*t Go",
     author: "Monica Sweeney",
-    id: 1,
+    id: img1,
   },
-
   {
     img: "https://images-na.ssl-images-amazon.com/images/I/51VkB7O0v4L._AC_UL200_SR200,200_.jpg",
     title: "Good Days Start With Gratitude",
     author: "Pretty Simple Press",
-    id: 2,
+    id: img2,
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/41UG6tNeHBL._SX334_BO1,204,203,200_.jpg",
+    title: "Lessons in Chemistry: A Novel",
+    author: "Bonnie Garmus",
+    id: img3,
   },
 ];
 
