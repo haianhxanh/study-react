d80efa948344dbb2d5ed64e1000bd5b0e5b1c42d
Hai Anh <67957887+haianhxanh@users.noreply.github.com>
2023-02-14 23:59:51 +0100

    Move HTML into modules

---
+++ react-tutorials/coding-addict/fundamentals/backroads-app/src/components/Navbar.js
@@ -0,0 +1,80 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src="./images/logo.svg" className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+        <ul className="nav-links" id="nav-links">
+          <li>
+            <a href="#home" className="nav-link">
+              {" "}
+              home{" "}
+            </a>
+          </li>
+
+          <li>
+            <a href="#about" className="nav-link">
+              {" "}
+              about{" "}
+            </a>
+          </li>
+
+          <li>
+            <a href="#services" className="nav-link">
+              {" "}
+              services{" "}
+            </a>
+          </li>
+
+          <li>
+            <a href="#tours" className="nav-link">
+              {" "}
+              tours
+            </a>
+          </li>
+        </ul>
+
+        <ul className="nav-icons">
+          <li>
+            <a
+              href="https://www.twitter.com"
+              target="_blank"
+              className="nav-icon"
+              rel="noreferrer"
+            >
+              <i className="fab fa-facebook"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.twitter.com"
+              target="_blank"
+              className="nav-icon"
+              rel="noreferrer"
+            >
+              <i className="fab fa-twitter"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.twitter.com"
+              target="_blank"
+              className="nav-icon"
+              rel="noreferrer"
+            >
+              <i className="fab fa-squarespace"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
