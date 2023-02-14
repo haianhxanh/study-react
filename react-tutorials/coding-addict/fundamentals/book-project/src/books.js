1e2e39fae8574f8d94433d4dd325aef4fac2535d
Hai Anh <67957887+haianhxanh@users.noreply.github.com>
2023-02-14 21:59:04 +0100

    ES6 modules

---
+++ react-tutorials/coding-addict/fundamentals/book-project/src/books.js
@@ -0,0 +1,33 @@
+export const books = [
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/71hTq4t1PEL._AC_UL200_SR200,200_.jpg",
+    title: "Let That Sh*t Go",
+    author: "Monica Sweeney",
+    id: 1,
+  },
+
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/51VkB7O0v4L._AC_UL200_SR200,200_.jpg",
+    title: "Good Days Start With Gratitude",
+    author: "Pretty Simple Press",
+    id: 2,
+  },
+];
+
+// const books = [
+//   {
+//     img: "https://images-na.ssl-images-amazon.com/images/I/71hTq4t1PEL._AC_UL200_SR200,200_.jpg",
+//     title: "Let That Sh*t Go",
+//     author: "Monica Sweeney",
+//     id: 1,
+//   },
+
+//   {
+//     img: "https://images-na.ssl-images-amazon.com/images/I/51VkB7O0v4L._AC_UL200_SR200,200_.jpg",
+//     title: "Good Days Start With Gratitude",
+//     author: "Pretty Simple Press",
+//     id: 2,
+//   },
+// ];
+
+// export default books;
--- react-tutorials/coding-addict/fundamentals/book-project/src/index.js
+++ react-tutorials/coding-addict/fundamentals/book-project/src/index.js
@@ -3,54 +3,17 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const books = [
-  {
-    img: "https://images-na.ssl-images-amazon.com/images/I/71hTq4t1PEL._AC_UL200_SR200,200_.jpg",
-    title: "Let That Sh*t Go",
-    author: "Monica Sweeney",
-    id: 1,
-  },
-
-  {
-    img: "https://images-na.ssl-images-amazon.com/images/I/51VkB7O0v4L._AC_UL200_SR200,200_.jpg",
-    title: "Good Days Start With Gratitude",
-    author: "Pretty Simple Press",
-    id: 2,
-  },
-];
+import { books } from "./books";
+import Book from "./Book";
 
 const BookList = () => {
-  const getBook = (id) => {
-    const book = books.find((book) => book.id == id);
-    console.log(book);
-  };
-
   return (
     <section className="booklist">
       {books.map((book) => {
         // const { img, title, author, id } = book;
-        return <Book {...book} key={book.id} getBook={getBook} />;
+        return <Book {...book} key={book.id} />;
       })}
     </section>
-  );
-};
-
-const Book = (props) => {
-  const { id, img, title, author, getBook } = props;
-
-  const getSingleBook = () => {
-    getBook(id);
-  };
-
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      {/*  */}
-      {/* <button onClick={getSingleBook}>Click me</button> */}
-      <button onClick={() => getBook(id)}>Click me</button>
-      <h4>{author}</h4>
-    </article>
   );
 };
 
