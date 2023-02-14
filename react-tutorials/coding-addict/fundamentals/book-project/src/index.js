01698fd789016a42a6fa29e4f55b8f575c814081
Hai Anh <67957887+haianhxanh@users.noreply.github.com>
2023-02-14 18:58:26 +0100

    Event

--- react-tutorials/coding-addict/fundamentals/book-project/src/index.js
+++ react-tutorials/coding-addict/fundamentals/book-project/src/index.js
@@ -1,1 +1,83 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+const books = [
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/71hTq4t1PEL._AC_UL200_SR200,200_.jpg",
+    title: "Let That Sh*t Go",
+    author: "Monica Sweeney",
+    id: 1,
+  },
+
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/51VkB7O0v4L._AC_UL200_SR200,200_.jpg",
+    title: "Good Days Start With Gratitude",
+    author: "Pretty Simple Press",
+    id: 2,
+  },
+];
+
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <EventExamples />
+      {books.map((book) => {
+        // const { img, title, author, id } = book;
+        return <Book {...book} key={book.id} />;
+      })}
+    </section>
+  );
+};
+
+const EventExamples = () => {
+  const handleFormInput = () => {
+    console.log("handle form input");
+  };
+
+  const handleButtonClick = () => {
+    console.log("handle button click");
+  };
+
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    console.log("form submit");
+  };
+
+  return (
+    <section>
+      <form onSubmit={handleFormSubmission}>
+        <h2>Form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={handleFormInput}
+          style={{ margin: "1rem 0" }}
+        />
+        <button type="submit">Click me</button>
+        <button
+          onClick={() => {
+            console.log("anonymous fce");
+          }}
+        >
+          Click me
+        </button>
+      </form>
+    </section>
+  );
+};
+
+const Book = (props) => {
+  const { img, title, author } = props;
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList />);
