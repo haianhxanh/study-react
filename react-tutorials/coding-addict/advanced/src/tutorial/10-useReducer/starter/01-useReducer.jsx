cde6f4c989f1637428e9086b86bc0820cd7066b3
Hai Anh <67957887+haianhxanh@users.noreply.github.com>
2023-03-26 23:27:07 +0200

    useReducer

--- react-tutorials/coding-addict/advanced/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ react-tutorials/coding-addict/advanced/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,24 +1,60 @@
 import React from "react";
 import { data } from "../../../data";
+import { useReducer } from "react";
+
+// default state
+const defaultState = {
+  people: data,
+};
+
+const CLEAR_LIST = "CLEAR_LIST";
+const RESET_LIST = "RESET_LIST";
+const REMOVE_ITEM = "REMOVE_ITEM";
+
+// reducer - controlling entire state
+// 1st param: state (before the update), 2nd param: action
+const reducer = (state, action) => {
+  if (action.type === CLEAR_LIST) {
+    // copy current state, return new one
+    return { ...state, people: [] };
+  }
+  if (action.type === RESET_LIST) {
+    return { ...state, people: data };
+  }
+  if (action.type === REMOVE_ITEM) {
+    let newPeople = state.people.filter(
+      (person) => person.id !== action.payload.id
+    );
+    return { ...state, people: newPeople };
+  }
+  throw new Error(`No matching ${action.type} - action type`);
+};
+
 const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  // const [people, setPeople] = React.useState(data);
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    // let newPeople = people.filter((person) => person.id !== id);
+    // setPeople(newPeople);
+    dispatch({ type: REMOVE_ITEM, payload: { id } });
   };
 
   const clearList = () => {
-    setPeople([]);
+    // dispatch looking for an object, always pass in type in string/variable of string type
+    dispatch({ type: CLEAR_LIST });
+    // setPeople([]);
   };
 
   const resetList = () => {
-    setPeople(data);
+    dispatch({ type: RESET_LIST });
+    // setPeople(data);
   };
 
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
           <div key={id} className="item">
@@ -28,7 +64,7 @@ const ReducerBasics = () => {
         );
       })}
 
-      {people.length < 1 ? (
+      {state.people.length < 1 ? (
         <button
           className="btn"
           style={{ marginTop: "2rem" }}
