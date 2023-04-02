f86ef404a2813efd037d9a5f732cd5c14906f3be
Hai Anh <67957887+haianhxanh@users.noreply.github.com>
2023-04-02 12:14:57 +0200

    Project Tours

---
+++ react-tutorials/coding-addict/projects-v3/02-tours/starter/src/Tour.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const Tour = ({ id, image, info, name, price, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} className="img" />
+      <span className="tour-price">${price}</span>
+      <div className="tour-info">
+        <h5>{name}</h5>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+
+          <button
+            type="button"
+            className="info-btn"
+            onClick={() => setReadMore(!readMore)}
+          >
+            {readMore ? "show less" : "read more"}
+          </button>
+        </p>
+        <button
+          className="btn btn-block delete-btn"
+          type="button"
+          onClick={() => removeTour(id)}
+        >
+          no interested/collectives
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default Tour;
